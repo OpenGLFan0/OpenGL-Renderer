d84b50056ad49660f395d4bd531082d0ff1da504
OpenGLFan0 <55481715+OpenGLFan0@users.noreply.github.com>
2025-10-16 22:43:31 +0800

    Update main.c

--- OpenGL Renderer/main.c
+++ OpenGL Renderer/main.c
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <math.h>
-//glad -o main main.c glad.c -I./include -lglfw -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -z noexecstack
+//clang -o main main.c glad.c -I./include -lglfw -lGL -lm -lXrandr -lXi -lX11 -lXxf86vm -lpthread -ldl -lXinerama -lXcursor -z noexecstack
 
 int main(int argc, char argv){
     glfwInit();
